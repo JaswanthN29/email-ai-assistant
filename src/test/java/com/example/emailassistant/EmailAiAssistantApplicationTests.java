0268381e0e73a585207c91140b1376bedb21df0a
Jaswanth's MacBook <naraharisettyjaswanth@gmail.com>
2026-05-30 13:43:09 +0530

    Add full project files

---
+++ src/test/java/com/example/emailassistant/EmailAiAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emailassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailAiAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
